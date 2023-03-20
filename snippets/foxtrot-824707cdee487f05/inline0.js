e286e60330105cd912a52057d62a19a8dfb32da6
janhohenheim <jan@hohenheim.ch>
2023-03-20 15:53:04 +0000

    Deploying to gh-pages from @ janhohenheim/foxtrot@b14c383d8288f05a4e4c329dc21bc4509c0b4fa9 🚀

--- snippets/foxtrot-824707cdee487f05/inline0.js
+++
@@ -1,7 +0,0 @@
-
-        export function show_loader() {
-            document.querySelector('.loader').hidden = false;
-        }
-        export function hide_loader() {
-            document.querySelector('.loader').hidden = true;
-        }
